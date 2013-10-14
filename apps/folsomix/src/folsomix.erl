ac013af8ea252fdad533f0cd26dfc8c63533763f
Denis Fakhrtdinov <me@shizz.ru>
2013-10-14 04:47:43 +0400

    dump  metrics except history

--- apps/folsomix/src/folsomix.erl
+++ apps/folsomix/src/folsomix.erl
@@ -4,7 +4,8 @@
     join_lines/1,
     get_metrics_by_type/1,
     any_to_binary/1,
-    list_metric_to_binary/1,
+    list_to_binary2/1,
+    list_to_binary2/2,
     type_to_atom/1
 ]).
 
@@ -43,7 +44,7 @@ any_to_binary(Metric) when is_binary(Metric) ->
     Metric;
 
 any_to_binary(Metric) when is_list(Metric) ->
-    list_metric_to_binary(Metric, <<>>);
+    list_to_binary2(Metric);
 
 any_to_binary(Metric) when is_atom(Metric) ->
     atom_to_binary(Metric, latin1);
@@ -51,22 +52,28 @@ any_to_binary(Metric) when is_atom(Metric) ->
 any_to_binary(Metric) when is_integer(Metric) ->
     list_to_binary(integer_to_list(Metric));
 
+any_to_binary(Metric) when is_float(Metric) ->
+    list_to_binary(io_lib:format("~f", [Metric]));
+
 any_to_binary(Metric) ->
-    throw({canno_convert_to_binary, Metric}).
+    throw({cannot_convert_to_binary, Metric}).
 
 
 
-list_metric_to_binary(List) ->
-    list_metric_to_binary(List, <<>>).
+list_to_binary2(List) ->
+    list_to_binary2(List, <<".">>).
 
-list_metric_to_binary([], Acc) ->
+list_to_binary2(List, Delimiter) ->
+    list_to_binary2(List, Delimiter, <<>>).
+
+list_to_binary2([], _Delimiter, Acc) ->
     Acc;
 
-list_metric_to_binary([Metric | List], <<>>) ->
-    list_metric_to_binary(List, any_to_binary(Metric));
+list_to_binary2([Metric | List], Delimiter, <<>>) ->
+    list_to_binary2(List, Delimiter, any_to_binary(Metric));
 
-list_metric_to_binary([Metric | List], Acc) ->
-    list_metric_to_binary(List, <<Acc/binary, ".", (any_to_binary(Metric))/binary>>).
+list_to_binary2([Metric | List], Delimiter, Acc) ->
+    list_to_binary2(List, Delimiter, <<Acc/binary, Delimiter/binary, (any_to_binary(Metric))/binary>>).
 
 
 
@@ -75,6 +82,6 @@ type_to_atom(<<"duration", _/binary>>) -> duration;
 type_to_atom(<<"gauge", _/binary>>) -> gauge;
 type_to_atom(<<"histogram", _/binary>>) -> histogram;
 type_to_atom(<<"history", _/binary>>) -> history;
-type_to_atom(<<"meter", _/binary>>) -> meter;
 type_to_atom(<<"meter_reader", _/binary>>) -> meter_reader;
+type_to_atom(<<"meter", _/binary>>) -> meter;
 type_to_atom(<<"spiral", _/binary>>) -> spiral.
--- apps/folsomix/src/folsomix_dump.erl
+++ apps/folsomix/src/folsomix_dump.erl
@@ -8,8 +8,8 @@
 
 
 
-handle(Socket, counter) ->
-    Metrics = folsomix:get_metrics_by_type(counter),
+handle(Socket, Type) when gauge == Type; counter == Type ->
+    Metrics = folsomix:get_metrics_by_type(Type),
     Lines = lists:foldl(fun(Metric, Acc) ->
         Line = <<
             (folsomix:any_to_binary(Metric))/binary,
@@ -21,9 +21,86 @@ handle(Socket, counter) ->
     gen_tcp:send(Socket, folsomix:join_lines(Lines)),
     ok;
 
-handle(Socket, Type) ->
-    io:format("Dump of metrics type ~p~n", [Type]),
-    gen_tcp:send(Socket, <<"Ok, thanks">>),
+handle(Socket, histogram) ->
+    Metrics = folsomix:get_metrics_by_type(histogram),
+    Lines = lists:foldl(fun(Metric, Acc) ->
+        Line = <<
+            (folsomix:any_to_binary(Metric))/binary,
+            "\t",
+            (folsomix:list_to_binary2(folsom_metrics:get_metric_value(Metric), <<" ">>))/binary
+        >>,
+        [Line | Acc]
+    end, [], Metrics),
+    gen_tcp:send(Socket, folsomix:join_lines(Lines)),
+    ok;
+
+handle(Socket, meter) ->
+    Metrics = folsomix:get_metrics_by_type(meter),
+    Lines = lists:foldl(fun(Metric, Acc) ->
+        MetricValue = folsom_metrics:get_metric_value(Metric),
+        Count = proplists:get_value(count, MetricValue),
+        One = proplists:get_value(one, MetricValue),
+        Five = proplists:get_value(five, MetricValue),
+        Fifteen = proplists:get_value(fifteen, MetricValue),
+        Day = proplists:get_value(day, MetricValue),
+        Mean = proplists:get_value(mean, MetricValue),
+        Acceleration = proplists:get_value(acceleration, MetricValue),
+        OneToFive = proplists:get_value(one_to_five, Acceleration),
+        FiveToFifteen = proplists:get_value(five_to_fifteen, Acceleration),
+        OneToFifteen = proplists:get_value(one_to_fifteen, Acceleration),
+
+        Line = <<
+            (folsomix:any_to_binary(Metric))/binary,
+            "\t",
+            (folsomix:list_to_binary2([Count, One, Five, Fifteen, Day, Mean, OneToFive, FiveToFifteen, OneToFifteen], <<" ">>))/binary
+        >>,
+        [Line | Acc]
+    end, [], Metrics),
+    gen_tcp:send(Socket, folsomix:join_lines(Lines)),
+    ok;
+
+handle(Socket, meter_reader) ->
+    Metrics = folsomix:get_metrics_by_type(meter_reader),
+    Lines = lists:foldl(fun(Metric, Acc) ->
+        MetricValue = folsom_metrics:get_metric_value(Metric),
+        One = proplists:get_value(one, MetricValue),
+        Five = proplists:get_value(five, MetricValue),
+        Fifteen = proplists:get_value(fifteen, MetricValue),
+        Mean = proplists:get_value(mean, MetricValue),
+        Acceleration = proplists:get_value(acceleration, MetricValue),
+        OneToFive = proplists:get_value(one_to_five, Acceleration),
+        FiveToFifteen = proplists:get_value(five_to_fifteen, Acceleration),
+        OneToFifteen = proplists:get_value(one_to_fifteen, Acceleration),
+
+        Line = <<
+            (folsomix:any_to_binary(Metric))/binary,
+            "\t",
+            (folsomix:list_to_binary2([One, Five, Fifteen, Mean, OneToFive, FiveToFifteen, OneToFifteen], <<" ">>))/binary
+        >>,
+        [Line | Acc]
+    end, [], Metrics),
+    gen_tcp:send(Socket, folsomix:join_lines(Lines)),
+    ok;
+
+handle(Socket, spiral) ->
+    Metrics = folsomix:get_metrics_by_type(spiral),
+    Lines = lists:foldl(fun(Metric, Acc) ->
+        MetricValue = folsom_metrics:get_metric_value(Metric),
+        Count = proplists:get_value(count, MetricValue),
+        One = proplists:get_value(one, MetricValue),
+
+        Line = <<
+            (folsomix:any_to_binary(Metric))/binary,
+            "\t",
+            (folsomix:list_to_binary2([Count, One], <<" ">>))/binary
+        >>,
+        [Line | Acc]
+    end, [], Metrics),
+    gen_tcp:send(Socket, folsomix:join_lines(Lines)),
+    ok;
+
+handle(Socket, _Type) ->
+    gen_tcp:send(Socket, <<"%UNSUPPORTED%">>),
     ok.
 
 
